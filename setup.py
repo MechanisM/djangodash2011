e9d28f386daa0ccd86a0188c566c270266eb6db7
Valya Golev <me@valyagolev.net>
2011-07-31 07:10:37 +0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(name='staste',
-      version='0.1',
+      version='0.1.1d',
       description="Slightly complicated event tracker for your Django website.",
       author='Valentin Golev',
       author_email='v.golev@gmail.com',
       url='http://staste.unfoldthat.com/',
-      packages=['staste'],
+      packages=['staste', 'staste.charts'],
+      package_data={'staste':
+                        ['templates/staste/*.html',
+                         'templates/staste/*/*.html']
+                    },
      )
