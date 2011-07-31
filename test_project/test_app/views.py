93de6d71108d9d1bb973ac49276b3ce87d99a76d
Valya Golev <me@valyagolev.net>
2011-07-31 07:32:58 +0400

    Redis mem usage

--- test_project/test_app/views.py
+++ test_project/test_app/views.py
@@ -1,7 +1,7 @@
-from datetime import datetime
-
 from django.http import HttpResponsePermanentRedirect
 from django.views.generic import FormView
+
+from staste import redis
 
 from .forms import ParticipantForm, GENDERS
 from .metrics import gender_age_metrica
@@ -10,10 +10,16 @@ from .metrics import gender_age_metrica
 class IndexView(FormView):
     form_class = ParticipantForm
     template_name = 'test_app/index.html'
+
+    def get_context_data(self, *args, **kwargs):
+        data = super(IndexView, self).get_context_data(*args, **kwargs)
+
+        data['redis_memory'] = redis.info()['used_memory_human']
+
+        return data
       
     def form_valid(self, form):
         gender_age_metrica.kick(
-                            date=datetime.now(), 
                             gender=form.cleaned_data['gender'],
                             age=form.cleaned_data['age']
                          )
